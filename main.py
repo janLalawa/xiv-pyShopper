da730ab0c45a1a0ec24441fa77592c994e99feba
Laura <40493346+Laurapaws@users.noreply.github.com>
2022-10-20 02:02:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import requests
+import json
+
+def main():
+    
+    makeplace_json = 'SaveMeiya.json'
+    furniture_data = json.load(open(makeplace_json))
+    
+    class Item:
+        def __init__(self, name, itemid, quantity, price_list, world_list, not_on_market):
+            self.name = name
+            self.itemid = itemid
+            self.quantity = quantity
+            self.price_list = price_list
+            self.world_list = world_list
+            self.not_on_market = not_on_market
+        
+        def iterate(self):
+            self.quantity += 1
+            
+        def flag(self):
+            self.not_on_market = 1 
+             
+                
+    def parse_shoppinglist(data):
+        
+        itemid_list = []
+        object_list = []
+        
+        for furniture in data["interiorFurniture"]:
+            
+            if furniture["itemId"] in itemid_list:
+                itemid_index = itemid_list.index(furniture["itemId"])
+                object_list[itemid_index].iterate()
+            else:
+                itemid_list.append(furniture["itemId"])
+                object_list.append(Item(furniture["name"], furniture["itemId"], 1, [], [], 0))
+        
+        return object_list, itemid_list
+    
+    def flag_unresolved(price_list):
+        
+        for obj in furniture:
+            if obj.itemid in price_list["unresolvedItems"]:
+                obj.flag()
+    
+    def get_prices(world_dc_region):
+        
+        total_cost = 0
+        unique_worlds = set()
+        
+        largest_quantity = []
+        for obj in furniture:
+            largest_quantity.append(obj.quantity)
+        largest_quantity = max(largest_quantity)
+        
+        itemid_string = ','.join(str(x) for x in itemids)
+        
+        universalis_query = 'https://universalis.app/api/v2/{location}/{item_list}?listings={listings}&entries=0'            
+        price_request = requests.get(universalis_query.format(location=world_dc_region, item_list=itemid_string, listings=largest_quantity))
+        
+        prices = price_request.json()
+        flag_unresolved(prices)
+        
+        for obj in furniture:
+            if obj.not_on_market == 0:
+                #print('working on' + obj.name)
+                temp_item_id = str(obj.itemid)
+                for listing in prices["items"][temp_item_id]["listings"][:obj.quantity]:
+                    obj.price_list.append(listing["pricePerUnit"])
+                    total_cost = total_cost + listing["pricePerUnit"]
+                    obj.world_list.append(listing["worldName"])
+                    unique_worlds.add(listing["worldName"])
+                    
+                    
+                        
+        
+        return total_cost, unique_worlds           
+    
+       
+    furniture, itemids = parse_shoppinglist(furniture_data)
+    total_cost, unique_worlds = get_prices('Light')
+    print(f"Total Cost: {total_cost:,} gil")
+    print(f"Items found on {unique_worlds}")
+    
+           
+
+if __name__ == '__main__':
+    main()
