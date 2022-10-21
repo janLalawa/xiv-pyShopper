806bc3261bbacbb2d5d6807d3695c711562062a3
Laura <40493346+Laurapaws@users.noreply.github.com>
2022-10-21 01:07:05 +0100

    Added World object and world shopping lists
    Basic print() setup for now until I work out something nicer looking

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ def main():
             
         def flag(self):
             self.not_on_market = 1 
+            
+    class World:
+        def __init__(self, name, furniture_totals, world_total_price):
+            self.name = name
+            self.furniture_totals = furniture_totals
+            self.world_total_price = world_total_price
+        
+        def add_to_totals(self, furniture_name, furniture_count):
+            self.furniture_totals.append([furniture_name, furniture_count])
+            
+        def add_to_price(self, amount):
+            self.world_total_price = self.world_total_price + amount
              
                 
     def parse_shoppinglist(data):
@@ -48,6 +60,7 @@ def main():
         
         total_cost = 0
         unique_worlds = set()
+        not_on_market = []
         
         largest_quantity = []
         for furniture in furnitures:
@@ -64,22 +77,61 @@ def main():
         
         for furniture in furnitures:
             if furniture.not_on_market == 0:
-                #print('working on' + obj.name)
+                #print('Working on ' + furniture.name)
                 temp_item_id = str(furniture.itemid)
                 for listing in prices["items"][temp_item_id]["listings"][:furniture.quantity]:
                     furniture.price_list.append(listing["pricePerUnit"])
                     total_cost = total_cost + listing["pricePerUnit"]
                     furniture.world_list.append(listing["worldName"])
                     unique_worlds.add(listing["worldName"])
+            else:
+                not_on_market.append([furniture.name, furniture.itemid])
         
-        return total_cost, unique_worlds           
+        return total_cost, unique_worlds, not_on_market            
     
+    def create_worlds(furnitures, unique_worlds):
+        
+        world_list = []
+        counter = -1
+        
+        for worldname in unique_worlds:
+            counter += 1
+            world_list.append(World(worldname, [], 0))
+            for furniture in furnitures:
+                world_item_count = furniture.world_list.count(worldname)
+                if world_item_count > 0:
+                    if furniture.not_on_market == 0:
+                        world_list[counter].add_to_price(int(sum(furniture.price_list)/world_item_count)) #Super lazy and innacurate I know
+                        world_list[counter].add_to_totals(furniture.name, world_item_count)
+                        
+        
+        return world_list
+                
+    def print_world_shopping_list(worlds):
+        
+        for world in worlds:
+            
+            print(f'Your shopping list for {world.name} will cost approx. {world.world_total_price:,} gil')
+            print('------------------------')
+            
+            for furniture in world.furniture_totals:
+                print(f'{furniture[1]}x {furniture[0]}')
+            print('------------------------')
+            print(' ')
+    
+        
        
     furnitures, itemids = parse_shoppinglist(furniture_data)
-    total_cost, unique_worlds = get_prices('Light')
+    total_cost, unique_worlds, not_on_market = get_prices('Light')
+    worlds = create_worlds(furnitures, unique_worlds)
+    
+    print_world_shopping_list(worlds)
+    
     print(f"Total Cost: {total_cost:,} gil")
     print(f"Items found on {unique_worlds}")
-    print("All done!")
+    print('')
+    print(f'Could not find these items on the market: {not_on_market}')    
+    print("Thank you for shopping with us today!")
            
 
 if __name__ == '__main__':
