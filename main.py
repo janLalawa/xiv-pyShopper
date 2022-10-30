b3dff5f599318770060844463f5200e3a19106cb
Frank Beckwith <beckwith.frank@gmail.com>
2022-10-30 13:30:45 -0600

    Refactor more stuff into smaller functions
    No idea if any of this works.  Won't know until Universalis quits having
    a tantrum.

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ class Item:
     def increment(self):
         self.quantity += 1
 
-    def flag(self):
+    def flag_not_on_market(self):
         self.not_on_market = True
+
+    def append_world_price(self, listing):
+        self.price_list.append(listing["pricePerUnit"])
+        self.world_list.append(listing["worldName"])
+    
+    def get_itemid_str(self):
+        return str(self.itemid)
 
 class World:
     def __init__(self, name, furniture_totals, world_total_price):
@@ -26,7 +33,7 @@ class World:
         self.furniture_totals.append([furniture_name, furniture_count])
 
     def add_to_price(self, amount):
-        self.world_total_price = self.world_total_price + amount
+        self.world_total_price += amount
 
 class Shopper:
     def __init__(self, data):
@@ -53,40 +60,40 @@ class Shopper:
     def flag_unresolved(self, price_list):
         for obj in self.furnitures:
             if obj.itemid in price_list["unresolvedItems"]:
-                obj.flag()
+                obj.flag_not_on_market()
     
     def get_prices(self, world_dc_region):
-        
+        prices = self.get_price_request(world_dc_region)
+        self.flag_unresolved(prices)
+        return self.process_price_listings(prices)
+
+    def process_price_listings(self, prices):
+        # maybe store these as member variables to be retrieved later
         total_cost = 0
         unique_worlds = set()
         not_on_market = []
-        
-        largest_quantity = []
-        for furniture in self.furnitures:
-            largest_quantity.append(furniture.quantity)
-        largest_quantity = max(largest_quantity)
-        
-        itemid_string = ','.join(str(x) for x in self.itemids)
-        
-        universalis_query = 'https://universalis.app/api/v2/{location}/{item_list}?listings={listings}&entries=0'            
-        price_request = requests.get(universalis_query.format(location=world_dc_region, item_list=itemid_string, listings=largest_quantity))
-        
-        prices = price_request.json()
-        self.flag_unresolved(prices)
         
         for furniture in self.furnitures:
             if furniture.not_on_market:
                 not_on_market.append([furniture.name, furniture.itemid])
             else:
-                #print('Working on ' + furniture.name)
-                temp_item_id = str(furniture.itemid)
-                for listing in prices["items"][temp_item_id]["listings"][:furniture.quantity]:
-                    furniture.price_list.append(listing["pricePerUnit"])
-                    total_cost = total_cost + listing["pricePerUnit"]
-                    furniture.world_list.append(listing["worldName"])
-                    unique_worlds.add(listing["worldName"])
-        
-        return total_cost, unique_worlds, not_on_market            
+                for listing in prices["items"][furniture.get_itemid_str()]["listings"][:furniture.quantity]:
+                    furniture.append_world_price(listing)
+                    total_cost += listing["pricePerUnit"]
+                    unique_worlds.add(listing["worldName"]) # replace with a `get_unique_worlds` call that loops furnitures to get worlds
+        return total_cost, unique_worlds, not_on_market
+
+    def get_price_request(self, world_dc_region):
+        largest_quantity = self.get_largest_quantity()
+        itemid_string = ','.join(str(x) for x in self.itemids)
+        universalis_query = 'https://universalis.app/api/v2/{location}/{item_list}?listings={listings}&entries=0'            
+        price_request = requests.get(universalis_query.format(
+            location=world_dc_region, item_list=itemid_string, listings=largest_quantity))
+        return price_request.json()
+
+    def get_largest_quantity(self):
+        largest_quantity = [furniture.quantity for furniture in self.furnitures]
+        return max(largest_quantity)
     
     def create_worlds(self, furnitures, unique_worlds):
         world_list = [World(name, [], 0) for name in unique_worlds]
@@ -115,7 +122,9 @@ class Shopper:
         worlds = self.create_worlds(self.furnitures, unique_worlds)
         
         self.print_world_shopping_list(worlds)
-        
+        self.print_footer(total_cost, unique_worlds, not_on_market)
+
+    def print_footer(self, total_cost, unique_worlds, not_on_market):
         print(f"Total Cost: {total_cost:,} gil")
         print(f"Items found on {unique_worlds}")
         print('')
